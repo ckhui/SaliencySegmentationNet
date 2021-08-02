e528162e5e23a136c965ba097ebf003146315687
CKHui <ckhanghui@gmail.com>
2021-08-03 00:39:11 +0800

    flask read request and response

--- flask/main.py
+++ flask/main.py
@@ -12,10 +12,11 @@ from io import BytesIO
 from flask import Flask
 from flask import request
 
-from deploy import loadModel, model_predict, get_peaksBB, crop, toSalSegBase64, crop_b64, filter_candidate, project_to_crop_size
+from deploy import loadModel, model_predict, get_peaksBB, crop, toSalSegBase64, decodeSalSegBase64, crop_b64, filter_candidate, project_to_crop_size, calculate_target_feature_size
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 def read_image_from_bytes(img_bytes):
     try:
@@ -35,6 +36,14 @@ def response_builder(res_data):
 def hello():
     return f"Model is ready: {MODEL_WEIGHT}"
 
+@app.route('/demo', methods=['POST'])
+def test():
+    if request.method == 'POST':
+        print(request.form['crop_x'])
+        print(request.form['crop_y'])
+        print(request.files['input'])
+    return "Hello"
+
 @app.route('/sspredict', methods=['POST'])
 def predict():
     if request.method == 'POST':
@@ -43,6 +52,14 @@ def predict():
         if "input" not in request.files:
             res_data['err'] = "Input Error"
             return response_builder(res_data)
+
+        crop_w = request.form.get('crop_w', 0)
+        crop_h = request.form.get('crop_h', 0)
+        if crop_w == 0 or crop_h == 0:
+            res_data['err'] = "Invalid Crop Size, crop size cannot be zero"
+            return response_builder(res_data)
+        crop_size = (int(crop_w), int(crop_h))
+
 
         img_bytes = request.files["input"].read()
         image, msg = read_image_from_bytes(img_bytes)
@@ -54,16 +71,16 @@ def predict():
         salseg = toSalSegBase64(sal,seg)
         res_data['sal_seg'] = salseg
         peaks_bb = get_peaksBB(seg, sal)
-        peaks_data = [{
-            'peak': list(map(int,p)),
-            'rect': list(map(int,bb)),
-            } for (p,bb) in peaks_bb.items()]
-            
-        res_data['peaks_bb'] = peaks_data
+        res_data['peaks_bb'] = peak2json(peaks_bb)
 
         # crop
-        # scores_xyxy = filter_candidate(scores_xyxy, 0.1)
-        # project_to_crop_size(scores_xyxy, target_size, crop_size)
+        h,w,_ = image.shape
+        h_feat, w_feat = seg.shape
+        target_size = calculate_target_feature_size((w,h), (w_feat,h_feat), crop_size)
+        scores_xyxy = crop(sal, seg, peaks_bb, target_size)
+        scores_xyxy = filter_candidate(scores_xyxy, 0.1)
+        scores_xyxy = project_to_crop_size(scores_xyxy, target_size, crop_size)
+        res_data['results'] = score2json(scores_xyxy)
         
         end = time.time()
         res_data['time'] = end-start
@@ -76,17 +93,51 @@ def predict():
 @app.route('/sscrop', methods=['POST'])
 def sscrop():
     if request.method == 'POST':
-        # get seg sal # base64
-        # get peaks_bb # array
-        # get src_size, crop_size calculate targetsize
-        
-        # h,w,_ = img.shape
-        # h_feat, w_feat = seg.shape
-        # target_size = calculate_target_feature_size((w,h), (w_feat,h_feat), crop_size)
-        # scores_xyxy = crop(sal, seg, peaks_bb)
-        res = {}
-        return response_builder(res)
+        res_data = {}
+        #  param check 
+        for k in ['sal_seg', 'peak_bb', 'img_w', 'img_h', 'crop_w', 'crop_h']:
+            if k not in request.json:
+                res_data['err'] = f"Missing Param [{k}]"
+                return response_builder(res_data)
 
+        seg_sal_b64 = request.json['sal_seg']
+        peaks_json = request.json['peak_bb']
+        peaks_bb = read_peaksjson(peaks_json)
+        img_w = int(request.json['img_w'])
+        img_h = int(request.json['img_h'])
+        crop_w = int(request.json['crop_w'])
+        crop_h = int(request.json['crop_h'])
+        img_size = (img_w, img_h)
+        crop_size = (crop_w, crop_h)
+        
+        seg_sal_b64 = seg_sal_b64.split(',')[-1]
+        sal, seg  = decodeSalSegBase64(seg_sal_b64)
+        h_feat, w_feat = seg.shape
+        target_size = calculate_target_feature_size(img_size, (w_feat,h_feat), crop_size)
+        scores_xyxy = crop(sal, seg, peaks_bb, target_size)
+        scores_xyxy = filter_candidate(scores_xyxy, 0.1)
+        scores_xyxy = project_to_crop_size(scores_xyxy, target_size, crop_size)
+        res_data['results'] = score2json(scores_xyxy)
+
+        return response_builder(res_data)
+
+def read_peaksjson(peaks_json):
+    return {tuple(p['peak']) : p['rect'] for p in peaks_json }
+
+def peak2json(peaks_bb):
+    peaks_data = [{
+        'peak': list(map(int,p)),
+        'rect': list(map(int,bb)),
+        } for (p,bb) in peaks_bb.items()]
+    return peaks_data
+
+def score2json(score_xyxy):
+    score_data = [{
+        'score': score,
+        'rect': list(map(int,xyxy)),
+        } for (score,xyxy) in score_xyxy]
+    return score_data
+        
 
 if __name__ == '__main__':
     print("Starting Server - Loading Model")
@@ -94,9 +145,9 @@ if __name__ == '__main__':
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')    
     # MODEL_WEIGHT = "../../weights/SSNET_best.pth"
     MODEL_WEIGHT = "../../weights/SSNET_ss_0.pth"
-    MODEL = loadModel(MODEL_WEIGHT, DEVICE)
+    # MODEL = loadModel(MODEL_WEIGHT, DEVICE)
     print("Starting Server - Model Loaded")
     
     print("Server Started")
-    app.run()
+    app.run(debug=True)
     
